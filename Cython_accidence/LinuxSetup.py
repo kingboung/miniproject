3c0ee001fd90af179dec578879796ff9731d8b2a
Kingboung <kingboung3102693@163.com>
2017-03-11 11:00:33 +0800

    build version3

--- Cython_accidence/LinuxSetup.py
+++ Cython_accidence/LinuxSetup.py
@@ -9,4 +9,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 #cythonize：编译源代码为C或C++，返回一个distutils Extension对象列表
-setup(ext_modules=cythonize('version2.pyx'))
+setup(ext_modules=cythonize('version3.pyx'))
