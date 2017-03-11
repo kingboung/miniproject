b2f46ed718f1adba120bd977e68a0ade543a7e8a
Kingboung <kingboung3102693@163.com>
2017-03-11 11:05:24 +0800

    correct the wrong thing： The setup.py can do Windows' setup in Windows' system and do Linux's setup in Linux's system

--- Cython_accidence/setup.py
+++ Cython_accidence/setup.py
@@ -2,11 +2,11 @@
 # -*- coding:utf-8 -*-
 
 # Run as:
-#    python LinuxSetup.py build   编译
-#    python LinuxSetup.py install 安装（效果同pip install xxx）
+#    python setup.py build   编译
+#    python setup.py install 安装（效果同pip install xxx）
 
 from distutils.core import setup
 from Cython.Build import cythonize
 
 #cythonize：编译源代码为C或C++，返回一个distutils Extension对象列表
-setup(ext_modules=cythonize('version3.pyx'))
+setup(ext_modules=cythonize('version2.pyx'))
