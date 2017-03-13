e926a93322d981035d82fd2c19575717cf62b2ee
Kingboung <kingboung3102693@163.com>
2017-03-13 19:48:31 +0800

    version5：C语言实现

---
+++ Cython_accidence/version5.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include <stdio.h>
+//#include <time.h>
+#define NUM 500000
+
+//这个版本C语言实现
+
+float great_circle(float lon1,float lat1,float lon2,float lat2){
+    float radius=3956.0;
+    float pi=3.14159265;
+    float x=pi/180.0;
+    float a,b,theta,c;
+
+    a=(90.0-lat1)*(x);
+    b=(90.0-lat2)*(x);
+    theta=(lon2-lon1)*(x);
+    c=acos((cos(a)*cos(b))+(sin(a)*sin(b)*cos(theta)));
+    return radius*c;
+}
+
+int main(){
+    int i;
+    float x;
+    //clock_t start, finish;
+    //double Total_time;
+    //start = clock();
+    for(i=0;i<=NUM;i++)
+        x=great_circle(-72.345,34.323,-61.823,54.826);
+    //finish = clock();
+    //Total_time = (double)(finish-start) / CLOCKS_PER_SEC;
+    //printf("%f sec",Total_time);
+    printf("%f",x);
+}
+
+
+//Linux环境下:gcc -lm -octest version5.c   （当前路径生成ctest.exe）
+//time ./ctest  （测试该模块运行所需时间，当然把代码注释部分加进来运行也能起到相同的效果）
